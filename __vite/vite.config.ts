7f4dfa104d7c72b9ee85c7558c4e8863941d174e
Djobbo <hi@djobbo.com>
2024-06-07 23:48:54 +0200

    stremio aliases

--- __vite/vite.config.ts
+++ __vite/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      stremio: path.resolve(__dirname, "./src"),
+      "stremio-router": path.resolve(__dirname, "./src/router"),
+    },
+  },
   plugins: [react()],
 })
