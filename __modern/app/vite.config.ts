17f0663402825cc5c3ee1fd40dddea17539ac202
Djobbo <hi@djobbo.com>
2024-06-11 21:29:47 +0200

    vite pwa

--- __modern/app/vite.config.ts
+++ __modern/app/vite.config.ts
@@ -3,6 +3,7 @@ import { execSync } from "node:child_process"
 import react from "@vitejs/plugin-react-swc"
 import path from "path"
 import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 import topLevelAwait from "vite-plugin-top-level-await"
 import wasm from "vite-plugin-wasm"
 
@@ -39,7 +40,73 @@ export default defineConfig({
       },
     ],
   },
-  plugins: [react(), wasm(), topLevelAwait()],
+  plugins: [
+    react(),
+    wasm(),
+    topLevelAwait(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicons/favicon.ico",
+        "images/icon.png",
+        "images/maskable_icon.svg",
+      ],
+      manifest: {
+        name: "Stremio Web",
+        short_name: "Stremio",
+        description: "Freedom To Stream",
+        background_color: "#161523",
+        theme_color: "#2a2843",
+        orientation: "any",
+        display: "standalone",
+        display_override: ["standalone"],
+        icons: [
+          {
+            src: "/images/icon.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/images/icon.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/images/maskable_icon.png",
+            sizes: "512x512",
+            purpose: "maskable",
+          },
+          {
+            src: "/images/maskable_icon.png",
+            sizes: "192x192",
+            purpose: "maskable",
+          },
+          {
+            src: "/favicons/favicon.ico",
+            sizes: "256x256",
+          },
+        ],
+        screenshots: [
+          {
+            src: `/screenshots/board_wide.webp`,
+            sizes: "1440x900",
+            type: "image/webp",
+            form_factor: "wide",
+            label: "Homescreen of Stremio",
+          },
+          {
+            src: `/screenshots/board_narrow.webp`,
+            sizes: "414x896",
+            type: "image/webp",
+            form_factor: "narrow",
+            label: "Homescreen of Stremio",
+          },
+        ],
+      },
+    }),
+  ],
   define: {
     "import.meta.env.SENTRY_DSN": process.env.SENTRY_DSN
       ? JSON.stringify(process.env.SENTRY_DSN)
